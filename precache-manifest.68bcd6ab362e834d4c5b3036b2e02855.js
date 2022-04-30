040601a4397b16b2e48b1b16df74bf50d7467637
brysbra <brysbra@users.noreply.github.com>
2022-04-30 16:14:54 +0000

    deploy: c64098384b6d2cbb22d4caafa008fb20521120bb

---
+++ precache-manifest.68bcd6ab362e834d4c5b3036b2e02855.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7c5fa0856bd38b7faf3aeaf768519f",
+    "url": "https://brysbra.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "2ffcd52145ebdb578b5b",
+    "url": "https://brysbra.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "8cd2ff85ab7853a62803",
+    "url": "https://brysbra.github.io/task-management-frontend/static/js/2.4c3d73cc.chunk.js"
+  },
+  {
+    "revision": "2ffcd52145ebdb578b5b",
+    "url": "https://brysbra.github.io/task-management-frontend/static/js/main.afbe231d.chunk.js"
+  },
+  {
+    "revision": "ffc514faceb657762fe4",
+    "url": "https://brysbra.github.io/task-management-frontend/static/js/runtime~main.598a26b2.js"
+  }
+]);
